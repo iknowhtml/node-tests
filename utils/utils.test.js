236a44b685e795224e81e4962755e51b16940dcb
Aki Gao <xag9bb@virginia.edu>
2017-06-06 23:44:04 -0400

    complete 53. using an assertion library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,16 +1,31 @@
 const utils = require('./utils');
-const chai = require('chai');
+const { expect } = require('chai');
 
 it('should add two numbers', () => {
   const res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res).to.equal(44).to.be.a('number');
 });
 
 it('should square a number', () => {
   const res = utils.square(3);
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}`);
-  }
+  expect(res).to.equal(9).to.be.a('number');
 });
+
+it('should verify first and last name are set', () => {
+  let user = { location: 'Virginia', age: 23 };
+  const res = utils.setName(user, 'Aki Gao');
+  expect(res).to.deep.include({
+    firstName: 'Aki',
+    lastName: 'Gao'
+  });
+});
+
+// it('should expect some values', () => {
+//   // expect(12).to.not.equal(12);
+//   // expect({name: 'Aki'}).to.deep.equal({name: 'Aki'});
+//   expect({
+//     name: 'Aki',
+//     age: '23',
+//     location: 'Virginia',
+//   }).to.deep.include({ age: '23' });
+// });
