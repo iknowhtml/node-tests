e1b470619995afd95d630a118151689341f0a18f
Aki Gao <xag9bb@virginia.edu>
2017-06-13 21:22:17 -0400

    complete 58. test spies

--- spies/app.test.js
+++ spies/app.test.js
@@ -2,9 +2,8 @@ const sinon = require('sinon');
 const chai = require('chai');
 chai.use(require('sinon-chai'));
 
-const app = require('./app');
 const db = require('./db');
-
+const app = require('./app');
 
 describe('App', () => {
   it('should call the spy correctly', () => {
@@ -13,6 +12,13 @@ describe('App', () => {
     chai.expect(spy).to.have.been.calledWithExactly('Aki', 23);
   });
   it('should call saveUser with user object', () => {
-    const spy = sinon.spy(db);
+    const spy = sinon.spy(db, 'saveUser');
+    const email = 'aki@gao.io';
+    const password = 'good4u';
+    app.handleSignup(email, password);
+    chai.expect(spy).to.have.been.calledWith({
+      email: 'aki@gao.io',
+      password: 'good4u'
+    });
   });
 });
